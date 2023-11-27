e5af57e3a0c1170b2b02dae9b0855d5c4a4b9c1e
Hassan Malik <148216077+shady070@users.noreply.github.com>
2023-11-28 01:32:21 +0500

    Update Buttons.tsx

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,20 +1,20 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const Buttons = () => {
   return (
     <div>
       <div>
-      <motion.button
-        initial={{ x: 100, opacity: 0 }}
-        whileInView={{ x: 0, opacity: 1, transition: { delay: 1.5, duration: 0.8 } }}
-        whileHover={{ scale: 1.1, x: -4, y: -4 }}
-        className='btn-text-grd text-xs w-[156.09px] h-[38.62px] bg-zinc-950 rounded-[289.66px] border border-zinc-500 backdrop-blur-[16.09px]'
-      >
-        Soulbound Launchpad
-      </motion.button>
+        <motion.button
+          animate={{ x: 100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1, transition: { delay: 1.5, duration: 0.8 } }}
+          whileHover={{ scale: 1.1, x: -4, y: -4 }}
+          className='btn-text-grd text-xs w-[156.09px] h-[38.62px] bg-zinc-950 rounded-[289.66px] border border-zinc-500 backdrop-blur-[16.09px]'
+        >
+          Soulbound Launchpad
+        </motion.button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Buttons
+export default Buttons;
