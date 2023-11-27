418f238ad5d4cea4c4b587a274996babb5da8897
Hassan Malik <148216077+shady070@users.noreply.github.com>
2023-11-28 02:00:48 +0500

    Update ProfileBox.tsx

--- components/ProfileBox.tsx
+++ components/ProfileBox.tsx
@@ -1,12 +1,22 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
-const ProfileBox = ({ title, imageSrc }) => {
-  return (
-    <motion.div initial={{ opacity:0, y:100 }} whileInView={{ opacity:1, y:0 }} transition={{delay:0.8, duration:1 }} className='w-[284px] h-20 bg-zinc-950 rounded-[55px] border border-zinc-500 backdrop-blur-[20px] btn-text-grd flex gap-[14.42px] px-[5.11px] mb-[24px] items-center'>
-    <img className='w-16 h-16 relative rounded-[131.07px]' src={imageSrc} alt="profile-img" />
-    <p>{title}</p>
-   </motion.div>
-  )
+interface ProfileBoxProps {
+  title: string;
+  imageSrc: string;
 }
 
-export default ProfileBox
+const ProfileBox: React.FC<ProfileBoxProps> = ({ title, imageSrc }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.8, duration: 1 }}
+      className='w-[284px] h-20 bg-zinc-950 rounded-[55px] border border-zinc-500 backdrop-blur-[20px] btn-text-grd flex gap-[14.42px] px-[5.11px] mb-[24px] items-center'
+    >
+      <img className='w-16 h-16 relative rounded-[131.07px]' src={imageSrc} alt="profile-img" />
+      <p>{title}</p>
+    </motion.div>
+  );
+};
+
+export default ProfileBox;
